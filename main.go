288eb6b14cf8fdb44ff9030f65624c0dfa5b7bd3
Ivan Skiridomov <ivan.skiridomov@gmail.com>
2022-12-03 12:51:31 +0200

    Init commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/uptrace/bunrouter"
+	"github.com/uptrace/bunrouter/extra/reqlog"
+)
+
+func main() {
+	router := bunrouter.New(
+		bunrouter.Use(reqlog.NewMiddleware(
+			reqlog.FromEnv("BUNDEBUG"),
+		)),
+		bunrouter.WithNotFoundHandler(notFoundHandler),
+		bunrouter.WithMethodNotAllowedHandler(methodNotAllowedHandler),
+	)
+
+	router.GET("/", indexHandler)
+	router.POST("/405", indexHandler) // to test methodNotAllowedHandler
+
+	router.WithGroup("/api", func(g *bunrouter.Group) {
+		g.GET("/users/:id", debugHandler)
+		g.GET("/users/current", debugHandler)
+		g.GET("/users/*path", debugHandler)
+	})
+
+	log.Println("listening on http://localhost:8080")
+	log.Println(http.ListenAndServe(":8080", router))
+}
+
+func indexHandler(w http.ResponseWriter, req bunrouter.Request) error {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(
+		w,
+		"<html>Hello from bun %s</html>",
+		req.URL.Path,
+	)
+	return nil
+}
+
+func debugHandler(w http.ResponseWriter, req bunrouter.Request) error {
+	return bunrouter.JSON(w, bunrouter.H{
+		"route":  req.Route(),
+		"params": req.Params().Map(),
+	})
+}
+
+func notFoundHandler(w http.ResponseWriter, req bunrouter.Request) error {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(
+		w,
+		"<html>BunRouter can't find a route that matches <strong>%s</strong></html>",
+		req.URL.Path,
+	)
+	return nil
+}
+
+func methodNotAllowedHandler(w http.ResponseWriter, req bunrouter.Request) error {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	fmt.Fprintf(
+		w,
+		"<html>BunRouter does have a route that matches <strong>%s</strong>, "+
+			"but it does not handle method <strong>%s</strong></html>",
+		req.URL.Path, req.Method,
+	)
+	return nil
+}
